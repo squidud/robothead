fafb168fabe1a7168398de160a15b3121e72da10
Matthew Reichard <squid05@hotmail.com>
2026-02-17 15:28:27 -0600

    requirements

--- servocontrols.py
+++ servocontrols.py
@@ -2,10 +2,10 @@ from gpiozero import AngularServo
 from time import sleep
 
 # Create servos on different GPIO pins
-eyeslr = AngularServo(17)  # eyes l/r
-eyesud = AngularServo(27)  # eyes up/down
-jaw = AngularServo(22)  # jaw
-necklr = AngularServo(5)   # neck l/r
+eyeslr = AngularServo(17, min_angle=0, max_angle=180)  # eyes l/r
+eyesud = AngularServo(27, min_angle=0, max_angle=180)  # eyes up/down
+jaw = AngularServo(22, min_angle=0, max_angle=180)  # jaw
+necklr = AngularServo(5, min_angle=0, max_angle=180)   # neck l/r
 
 # Control them using angles (0-180 degrees)
 eyeslr.angle = 90  
