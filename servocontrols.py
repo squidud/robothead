9702bbcf80afaa621b56694d0023b95ac9080235
Matthew Reichard <squid05@hotmail.com>
2026-02-17 18:07:53 -0600

    new servo

--- servocontrols.py
+++ servocontrols.py
@@ -1,24 +1,38 @@
-from gpiozero import AngularServo
+from rpi_hardware_pwm import HardwarePWM
 from time import sleep
 
-# Create servos on different GPIO pins
-eyeslr = AngularServo(17)  # eyes l/r
-eyesud = AngularServo(27)  # eyes up/down
-jaw = AngularServo(22)  # jaw
-necklr = AngularServo(5)   # neck l/r
+SERVO_HZ = 50
+MIN_DUTY = 2.5   # 0.5ms pulse = 0 degrees
+MAX_DUTY = 12.5  # 2.5ms pulse = 180 degrees
 
-# Control them using angles (0-180 degrees)
-eyeslr.angle = 90  
-eyesud.angle = 90   
-jaw.angle = 90    
-necklr.angle = 90   
+def angle_to_duty(angle):
+    return MIN_DUTY + (angle / 180.0) * (MAX_DUTY - MIN_DUTY)
+
+# Pi 5 hardware PWM via RP1 chip (chip=2)
+# Channel 0 = GPIO 12, Channel 1 = GPIO 13
+# Channel 2 = GPIO 18, Channel 3 = GPIO 19
+eyeslr = HardwarePWM(pwm_channel=0, hz=SERVO_HZ, chip=2)  # GPIO 12 - eyes l/r
+eyesud = HardwarePWM(pwm_channel=1, hz=SERVO_HZ, chip=2)  # GPIO 13 - eyes up/down
+jaw = HardwarePWM(pwm_channel=2, hz=SERVO_HZ, chip=2)     # GPIO 18 - jaw
+necklr = HardwarePWM(pwm_channel=3, hz=SERVO_HZ, chip=2)  # GPIO 19 - neck l/r
+
+# Start all servos at 90 degrees
+eyeslr.start(angle_to_duty(90))
+eyesud.start(angle_to_duty(90))
+jaw.start(angle_to_duty(90))
+necklr.start(angle_to_duty(90))
 
 sleep(1)
 
-while True:
-    eyeslr.angle = 0 
-    sleep(0.7)
+try:
+    while True:
+        eyeslr.change_duty_cycle(angle_to_duty(0))
+        sleep(0.9)
 
-    eyeslr.angle = 180
-    sleep(0.7)
-
+        eyeslr.change_duty_cycle(angle_to_duty(180))
+        sleep(0.9)
+except KeyboardInterrupt:
+    eyeslr.stop()
+    eyesud.stop()
+    jaw.stop()
+    necklr.stop()
