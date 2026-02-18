327d33fa1514cd7dce9377c244b6fb6666217940
Matthew Reichard <squid05@hotmail.com>
2026-02-17 18:15:14 -0600

    test

--- servocontrols.py
+++ servocontrols.py
@@ -8,13 +8,13 @@ MAX_DUTY = 12.5  # 2.5ms pulse = 180 degrees
 def angle_to_duty(angle):
     return MIN_DUTY + (angle / 180.0) * (MAX_DUTY - MIN_DUTY)
 
-# Pi 5 hardware PWM via RP1 chip (chip=2)
+# Pi 5 hardware PWM via RP1 chip
 # Channel 0 = GPIO 12, Channel 1 = GPIO 13
 # Channel 2 = GPIO 18, Channel 3 = GPIO 19
-eyeslr = HardwarePWM(pwm_channel=0, hz=SERVO_HZ, chip=2)  # GPIO 12 - eyes l/r
-eyesud = HardwarePWM(pwm_channel=1, hz=SERVO_HZ, chip=2)  # GPIO 13 - eyes up/down
-jaw = HardwarePWM(pwm_channel=2, hz=SERVO_HZ, chip=2)     # GPIO 18 - jaw
-necklr = HardwarePWM(pwm_channel=3, hz=SERVO_HZ, chip=2)  # GPIO 19 - neck l/r
+eyeslr = HardwarePWM(pwm_channel=0, hz=SERVO_HZ, chip=0)  # GPIO 12 - eyes l/r
+eyesud = HardwarePWM(pwm_channel=1, hz=SERVO_HZ, chip=0)  # GPIO 13 - eyes up/down
+jaw = HardwarePWM(pwm_channel=2, hz=SERVO_HZ, chip=0)     # GPIO 18 - jaw
+necklr = HardwarePWM(pwm_channel=3, hz=SERVO_HZ, chip=0)  # GPIO 19 - neck l/r
 
 # Start all servos at 90 degrees
 eyeslr.start(angle_to_duty(90))
